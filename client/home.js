fa4d17553432b2e989e717d50efb8ae7fb222933
David Higgins <thehig@gmail.com>
2014-11-25 22:35:45 +0000

    More commenting and refactoring

--- client/home.js
+++ client/home.js
@@ -1,11 +1,20 @@
+/**
+ * Get the other players ID from the given game
+ * @param game
+ * @returns {*} the other players id
+ */
 function otherId(game){
     return game.currentTurn[game.currentTurn[0] === Meteor.userId() ? 1 : 0]
 }
 
 Template.gameList.helpers({
+    //Provide a games object to the gameList template
     games: function(){
+        //Get only in-progress games
         return Games.find({inProgress: true}).map(function(game){
+            //Add an 'otherPlayer' name property (for UX/binding)
             game.otherPlayer = Meteor.users.findOne(otherId(game)).username;
+            //How long ago was the game started? (for UX/binding)
             game.started = moment(game.started).fromNow();
             return game;
         });
@@ -13,25 +22,27 @@ Template.gameList.helpers({
 });
 
 Template.userList.helpers({
+    //Provide users data for the userList template
     users: function(){
 
+        //You're not allowed play games against yourself
         var myId = Meteor.userId(),
             cantPlayAgainst = [myId];
 
+        //Find games that are in progress and accumulate the Ids of our current opponents
+        //  You are only allowed to have a single game against a given opponent
         Games.find({inProgress: true}).forEach(function(game){
             cantPlayAgainst.push(otherId(game));
         });
 
-
-
-        //MongoDB query
+        //MongoDB query to find us some games with people we CAN play against
         return Meteor.users.find({ _id: { $not: { $in: cantPlayAgainst}}});
     }
 });
 
 Template.userItem.events({
     'click button': function(evt, template){
-        //Meteor Methods, where a function is hidden securely in server, but can be called from client
+        //Securely call (via the Meteor.methods api) the createGame function with the opponent players Id
         Meteor.call('createGame', template.data._id);
     }
 });
--- client/login.js
+++ client/login.js
@@ -1,6 +1,12 @@
+//Note - Because we are making a single page application, we do not want the default form submission
+//          evt.preventDefault(); prevents the form from submitting and allows us to handle the event manually
+
 Template.signup.events({
     'click button': function(evt, template){
         evt.preventDefault();
+
+        //The Accounts plugin will automatically login a use who signs-up
+        //  This will only work if the boxes are sufficiently filled
         Accounts.createUser({
             email: template.find('#su-email').value,
             username: template.find('#su-username').value,
