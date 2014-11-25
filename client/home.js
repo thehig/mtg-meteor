edde7583def437477f6fe6e01d7fa2cf750a83fd
David Higgins <thehig@gmail.com>
2014-11-25 18:39:31 +0000

    Completed 3.2 Displaying the Game List Added list of games in progress

--- client/home.js
+++ client/home.js
@@ -1,3 +1,17 @@
+function otherId(game){
+    return game.currentTurn[game.currentTurn[0] === Meteor.userId() ? 1 : 0]
+}
+
+Template.gameList.helpers({
+    games: function(){
+        return Games.find({inProgress: true}).map(function(game){
+            game.otherPlayer = Meteor.users.findOne(otherId(game)).username;
+            game.started = moment(game.started).fromNow();
+            return game;
+        });
+    }
+});
+
 Template.userList.helpers({
     users: function(){
 
@@ -5,7 +19,7 @@ Template.userList.helpers({
             cantPlayAgainst = [myId];
 
         Games.find({inProgress: true}).forEach(function(game){
-            cantPlayAgainst.push(game.currentTurn[game.currentTurn[0] === myId ? 1 : 0]);
+            cantPlayAgainst.push(otherId(game));
         });
 
 
